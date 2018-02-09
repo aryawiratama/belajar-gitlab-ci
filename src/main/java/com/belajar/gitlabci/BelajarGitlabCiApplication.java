55f67bdce30e5592cb195e915cbb9e02ace49c84
aryawiratama89 <gede.arya.wiratama@gmail.com>
2018-02-09 23:32:10 +0700

    initial commit

---
+++ src/main/java/com/belajar/gitlabci/BelajarGitlabCiApplication.java
@@ -0,0 +1,12 @@
+package com.belajar.gitlabci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarGitlabCiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarGitlabCiApplication.class, args);
+	}
+}
